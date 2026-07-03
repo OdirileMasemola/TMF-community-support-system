47214dd0917d4353fb6b5ec171f8b061e5b51e5a
leratomokoe <leratomokoena442@gmail.com>
2026-07-03 13:54:10 +0200

    mmm

--- src/pages/public/AboutPage.tsx
+++ src/pages/public/AboutPage.tsx
@@ -2,9 +2,13 @@ import { PlaceholderPage } from "@/pages/public/PlaceholderPage";
 
 export function AboutPage() {
   return (
-    <PlaceholderPage
-      title="About the Foundation"
-      description="Learn about the Themba Molefe Foundation's mission, values, and community outreach programmes. Full content will be added here soon."
-    />
+    <div style={{ paddingTop: "60px", paddingLeft: "40px" }}>
+      
+      <PlaceholderPage
+        title="About the Foundation"
+        description="The Themba Molefe Foundation is an NGO whose purpose is to enhance the living standards of underprivilegedgit people residing in Katlehong. This organization has one single mission that aims at dealing with some of the major challenges that these communities face, such as poverty, lack of educational opportunities, and lack of resources. The main aim of the foundation is to lift up these communities by creating positive changes."
+      />
+
+    </div>
   );
 }
